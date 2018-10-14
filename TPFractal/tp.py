ce4d15a3435ab42493d50c903a3d7cc67d92c10d
damianciancio <damianciancio7@gmail.com>
2018-10-14 18:16:31 -0300

    Actualizo el tp. Estaba lo que no andaba.

--- TPFractal/tp.py
+++ TPFractal/tp.py
@@ -1,19 +1,22 @@
+import matplotlib.pyplot as plt
 
-def fractal(longitud = 150.0, y = 0, startx = 0):
+
+
+
+def fractal(longitud = 150.0, y = 50, startx = 0):
     if longitud < 1:
         return None
     else:
-        print(".-----------.")
-        print(longitud)
-        print(y)
-        print(startx)
         dibujar(longitud, y, startx)
         longitud = longitud / 3
         for j in [startx, startx + (longitud*2)]:
-            fractal(longitud, y + 10, j)
+            fractal(longitud, y - 10, j)
 
 def dibujar(longitud, y, startx):
+    plt.plot([startx,startx+longitud],[y,y],'k-')
     pass
 
 
+
 fractal()
+plt.show()
